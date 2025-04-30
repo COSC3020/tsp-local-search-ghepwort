a07b673e91c67f8e9b0cf622519096df31c86cd4
GageHepworth <gage.hepworth@gmail.com>
2025-04-30 17:02:27 -0600

    My swap function works

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function tsp_ls(distance_matrix) {
     return -1;
 }
+
+
+function swap(route, i, k) {
+
+    let route1 = route.slice(0, i);
+    let route2 = route.slice(i, k + 1);
+    let route3 = route.slice(k + 1)
+
+    route2 = route2.reverse();
+
+    return [...route1, ...route2, ...route3];
+}
+
+
+myRoute = ["A", "B", "C", "D", "E", "F"];
+
+
+console.log(swap(myRoute, 2, 4));
