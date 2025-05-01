458a6d26a2489cf6d314ee7d0f534ccd942bc2aa
GageHepworth <gage.hepworth@gmail.com>
2025-04-30 20:23:29 -0600

    When I run the test code via nodeJS it says it works and when I try it within the file it works, but it says it doesn't work when I push to github? I'm going to submit it to see if it is just an error on Github's side otherwise I appologize that I submitted non-working code.

--- code.js
+++ code.js
@@ -60,11 +60,11 @@ function swap(route, i, k) {
 
     // First and third part dont change
     let route1 = route.slice(0, i);
-    let route2 = route.slic
+    let route2 = route.slice(i, k + 1);
+    let route3 = route.slice(k + 1)
+
     // Flip the middle
     route2 = route2.reverse();
-    e(i, k + 1);
-    let route3 = route.slice(k + 1)
 
     // Obscure JS functions for the win!!!
     return [...route1, ...route2, ...route3];
